93bdb055df7e1d22e4795039beab4ad951087890
l_yunwen <l_yunwen@qq.com>
2016-01-25 11:16:51 +0800

    g9

--- BoooooJu.Web.Core.Test/GetUserTest.cs
+++ BoooooJu.Web.Core.Test/GetUserTest.cs
@@ -9,7 +9,7 @@ namespace BoooooJu.Web.Core.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string goooo = "3";
+            string goooo9 = "3";
             BoooooJuService.IGetUser client = new BoooooJuService.GetUserClient();
             var value = client.GetUserByAccount("liuyunwen13058");
         }
