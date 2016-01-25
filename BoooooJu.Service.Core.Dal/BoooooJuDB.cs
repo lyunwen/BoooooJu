b5b1684876ff643e1a76965e4e6aa57a417b1e41
l_yunwen <l_yunwen@qq.com>
2016-01-25 19:23:51 +0800

    fff

--- BoooooJu.Service.Core.Dal/BoooooJuDB.cs
+++ BoooooJu.Service.Core.Dal/BoooooJuDB.cs
@@ -1,9 +1,10 @@
-using System;
-using System.Data.Entity;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
 namespace BoooooJu.Service.Core.Dal
 {
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
     public partial class BoooooJuDB : DbContext
     {
         public BoooooJuDB()
@@ -11,8 +12,9 @@ namespace BoooooJu.Service.Core.Dal
         {
         }
 
-        public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<Test> Tests { get; set; }
+        public virtual DbSet<UerKey> UerKeys { get; set; }
+        public virtual DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
