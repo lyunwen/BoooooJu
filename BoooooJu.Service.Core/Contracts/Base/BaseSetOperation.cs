15b64df259294488f21ec6815d1da9e2407901b3
l_yunwen <l_yunwen@qq.com>
2016-01-25 11:35:14 +0800

    rty

--- BoooooJu.Service.Core/Contracts/Base/BaseSetOperation.cs
+++ BoooooJu.Service.Core/Contracts/Base/BaseSetOperation.cs
@@ -17,7 +17,7 @@ namespace BoooooJu.Service.Core.Contracts.Base
             T result = null;
             if (t != null)
             {
-                int i = 55;
+                int i = 554;
                 using (BoooooJuDB db = new BoooooJuDB())
                 {
                     db.Entry<T>(t).State = System.Data.Entity.EntityState.Added;
