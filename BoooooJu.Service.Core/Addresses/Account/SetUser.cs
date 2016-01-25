8e4102baaa1885f02ee2a4f152f11e92d870d375
l_yunwen <l_yunwen@qq.com>
2016-01-25 20:00:12 +0800

    go

--- BoooooJu.Service.Core/Addresses/Account/SetUser.cs
+++ BoooooJu.Service.Core/Addresses/Account/SetUser.cs
@@ -11,5 +11,33 @@ namespace BoooooJu.Service.Core.Addresses.Account
 {
     public class SetUser : Base.BaseSetData<User>, ISetUser
     {
+        public User RegisterUser(User user, string pswd)
+        {
+            User result = null;
+            if (user == null || pswd == null)
+                return result;
+            try
+            {
+                using (BoooooJuDB db = new BoooooJuDB())
+                {
+                    db.Users.Add(user);
+                    UserKey key = new UserKey
+                    {
+                        UserId = user.Id,
+                        Pswd = pswd,
+                        PswdSalt = "0",
+                        PswdType = 0
+                    };
+                    db.UserKeys.Add(key);
+                    db.SaveChanges();
+                    result = user;
+                }
+            }
+            catch
+            {
+               // ignore
+            }
+            return user;
+        } 
     }
 }
