b822f1fa3e2cab4ea8e3a98d91ac6ed395399ade
l_yunwen <l_yunwen@qq.com>
2016-01-26 15:39:06 +0800

    s

---
+++ BoooooJu.Service.Core/Addresses/Base/BaseGetData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using BoooooJu.Service.Core.Contracts.Base;
+using BoooooJu.Service.Core.QueryParameter.Base;
+using BoooooJu.Service.Core.Dal;
+using System.ServiceModel;
+
+namespace BoooooJu.Service.Core.Addresses.Base
+{
+    public class BaseGetData<T> : IBaseGetData<T> where T : class
+    {
+        public T GetDataByPrimaryKey(params object[] keyValues)
+        {
+            T result = null;
+            using (BoooooJuDB db = new BoooooJuDB())
+            {
+                result = db.Set<T>().Find(keyValues);
+            }
+            return result;
+        }
+        
+        public T GetDataByUniqueKey(string key)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public List<T> GetDatasBy(IPage page)
+        {
+            List<T> result = null;
+            if (page == null)
+                return result;
+
+            using (BoooooJuDB db = new BoooooJuDB())
+            {
+                if (page.IsPage)
+                {
+                    result = db.Set<T>().Skip((page.PageIndex - 1) * page.PageSize).Take(page.PageSize).ToList();
+                }
+                else
+                {
+                    result = db.Set<T>().ToList();
+                }
+            }
+            return result;
+        }
+    }
+}
